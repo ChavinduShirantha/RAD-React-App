ec222cf40e52535e63587baa2f5a596350bd5e72
ChavinduShirantha <chavindushirantha@gmail.com>
2023-12-04 10:14:30 +0530

    Added Basic config setup for tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ import './App.css';
 
 function App() {
   return (
-      <h1 className="text-5xl font-bold underline">
-        Hello world!
-      </h1>
+      <>
+        {/*Inline way of defining styles*/}
+        <div style={{backgroundColor: "red",
+          padding: 10, textAlign: "center"}}>
+          Inline CSS Box</div>
+        <br/>
+        {/*Using external CSS class of defining styles*/}
+        <div className="box">External CSS Box</div>
+        <br/>
+        {/*This is what tailwind CSS uses kind of atomic utility classes*/}
+        <div className="bg-red p-10 text-center">Tailwind CSS Utility Class Box</div>
+      </>
   );
 }
 
