67472fc52ca63d32d5fde0b8891e22888cf2588e
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-04 12:53:36 +0530

    Updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
       <>
-          {/*Inline way of defining styles*/}
-          <div style={{backgroundColor: "red",
-              padding: 10, textAlign: "center"}}>
-              Inline CSS Box</div>
-          <br/>
-          {/*Using external CSS class of defining styles*/}
-          <div className="box">External CSS Box</div>
-          <br/>
-          {/*This is what tailwind CSS uses kind of atomic utility classes*/}
-          <div className="bg-red p-10 text-center">Tailwind CSS Utility Class Box</div>
+
       </>
   );
 }
