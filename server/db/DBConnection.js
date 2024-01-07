51e604dced18b4e93f8cc640023b5d61de4c81c8
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-07 15:11:56 +0530

    Updated

---
+++ server/db/DBConnection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const db = async () => {
+    try {
+        const connect = await mongoose.connect(MONGODB_URL);
+        console.info(`Mongo DB connected to : ${connect.connection.host}`);
+    } catch (error) {
+        console.error("Mongo DB Connect Error : ", error)
+    }
+}
+
+module.exports = db();
