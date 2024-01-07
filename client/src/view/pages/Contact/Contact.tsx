6733dc727f9adbbd209789f1824c4cbf725c26da
ChavinduShirantha <chavindushirantha@gmail.com>
2024-01-07 15:36:31 +0530

    Updated

--- client/src/view/pages/Contact/Contact.tsx
+++ client/src/view/pages/Contact/Contact.tsx
@@ -1,6 +1,31 @@
 import {Component} from "react";
+import axios from "axios";
 
-export class Contact extends Component {
+interface ContactProps {
+    data: any
+}
+
+interface ContactState {
+    email: string,
+    subject: string,
+    message: string
+}
+
+
+export class Contact extends Component<ContactProps, ContactState> {
+    private api: any;
+
+    constructor(props: any) {
+        super(props);
+        this.api = axios.create({baseURL: `http://localhost:4000`});
+        this.state = {
+            email: '',
+            subject: '',
+            message: ''
+        }
+        this.handleMessageInputOnChange = this.handleMessageInputOnChange.bind(this);
+    }
+
     render() {
         return (
             <div className="flex">
@@ -21,24 +46,67 @@ export class Contact extends Component {
                     <form className="h-auto p-2 justify-center">
                         <div className="pb-2 flex flex-row">
                             <label className="text-[16px] basis-1/3">Your Email :</label>
-                            <input className="text-[16px] float-right border-[1px] border-green-400 basis-2/3 rounded" type="text"/>
+                            <input className="text-[16px] float-right border-[1px] border-green-400 basis-2/3 rounded"
+                                   type="text" name="email"
+                                   value={this.state.email}
+                                   // @ts-ignore
+                                   onChange={this.handleMessageInputOnChange}/>
                         </div>
                         <div className="pb-2 flex flex-row">
                             <label className="text-[16px] basis-1/3">Your Subject :</label>
-                            <input className="text-[16px] float-right border-[1px] border-green-400 basis-2/3 rounded" type="text"/>
+                            <input className="text-[16px] float-right border-[1px] border-green-400 basis-2/3 rounded"
+                                   type="text" name="subject"
+                                   value={this.state.subject}
+                                   // @ts-ignore
+                                   onChange={this.handleMessageInputOnChange}/>
                         </div>
                         <div className="pb-2 flex flex-row">
                             <label className="text-[16px] basis-1/3">Your Message :</label>
-                            <textarea className="h-24 text-[16px] float-right border-[1px] border-green-400 basis-2/3 rounded resize-none"/>
+                            <textarea
+                                className="h-24 text-[16px] float-right border-[1px] border-green-400 basis-2/3
+                                rounded resize-none" name="message"
+                                value={this.state.message}
+                                // @ts-ignore
+                                onChange={this.handleMessageInputOnChange}/>
                         </div>
 
                         <div className="mt-2 float-right">
-                            <button type="button" className="mt-5 pl-5 pr-5 pt-2 pb-2 text-white bg-green-400 text-[16px] rounded hover:text-tertiary">Sent Message
+                            <button type="button"
+                                    className="mt-5 pl-5 pr-5 pt-2 pb-2 text-white bg-green-400 text-[16px] rounded hover:text-tertiary"
+                                    onClick={this.onSendBtnClick}>Sent Message
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
         );
+    }
+
+    handleMessageInputOnChange(event: { target: { value: any; name: any; } }) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        // @ts-ignore
+        this.setState({
+            [name]: value
+        });
+    }
+    private onSendBtnClick = () => {
+        try {
+            this.api.post('/contact/submit',{
+                email:this.state.email,
+                subject:this.state.subject,
+                message:this.state.message
+            }).then((res:{data:any})=>{
+                const jsonData=res.data;
+                // @ts-ignore
+                alert(jsonData);
+            }).catch((error:any)=>{
+                console.error('Axios Error',error);
+            });
+        }catch (error){
+            console.error('Error Submitting Data',error);
+        }
     }
 }
--- server/app.js
+++ server/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
@@ -19,8 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
